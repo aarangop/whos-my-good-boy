e8b09e8568ebdeecd3693a34aa21a77e8d074fd2
Andres Arango <andresap@proton.me>
2025-04-28 15:53:05 -0500

    Create ModelLoaderManager, and add unit tests

--- backend/app/inference_models/model_loader.py
+++ backend/app/inference_models/model_loader.py
@@ -1,15 +1,10 @@
-
 import os
+from loguru import logger
 import tensorflow as tf
 from abc import ABC, abstractmethod
 
 
 class ModelLoader(ABC):
-
-    @abstractmethod
-    def download(self, source: str):
-        """Download the model from a specified path or source."""
-        pass
 
     @abstractmethod
     def load(self, model_filename: str):
@@ -28,9 +23,16 @@ class LocalModelLoader(ModelLoader):
     """
 
     def __init__(self, models_dir: str = './models'):
-        os.makedirs(models_dir, exist_ok=True)
+        if not os.path.exists(models_dir):
+            raise ValueError(
+                f"Models directory '{models_dir}' does not exist.")
+
         self.models_dir = models_dir
         self.models = {}
+
+        logger.info(
+            f"Instantiating {LocalModelLoader.__name__} with models directory '{self.models_dir}'"
+        )
 
     def load(self, model_filename: str):
         """Load the model from a local path."""
@@ -41,25 +43,13 @@ class LocalModelLoader(ModelLoader):
         model_path = os.path.join(self.models_dir, model_filename)
 
         if not os.path.exists(model_path):
-            raise FileNotFoundError(f"No model file '{self.model_path}'")
+            raise FileNotFoundError(f"No model file '{model_path}'")
 
-        model = tf.keras.models.load_model(self.model_path)
+        model = tf.keras.models.load_model(model_path)
         self.models[model_filename] = model
 
         return model
 
     def is_model_available(self):
-        return self.model is not None and isinstance(self.model, tf.keras.models.Model)
-
-
-class ModelService:
-    def __init__(self):
-        env = os.environ.get('ENVIRONMENT', 'development')
-
-        if env == 'development':
-            self.loader = LocalModelLoader(os.getenv("MODELS_DIR"))
-        else:
-            raise NotImplemented(f"Loader for {env} not implemented")
-
-    def get_model(self, model_filename: str):
-        self.loader.load(model_filename)
+        """Check if any models are loaded and available."""
+        return len(self.models) > 0 and all(isinstance(model, tf.keras.models.Model) for model in self.models.values())
